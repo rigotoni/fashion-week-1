6ee09d4ddfc86ce4737636b72084b3aa90ed658c
frami <i@2fd.me>
2022-03-22 12:41:10 -0300

    chore: add segment

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import Layout from './../components/Layout';
 
+const SEGMENT_KEY = process.env.NEXT_PUBLIC_SEGMENT_KEY
+
 const MyApp = ({ Component, pageProps }) => {
-  return (
+  return (<>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    {SEGMENT_KEY && (
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${SEGMENT_KEY}";;analytics.SNIPPET_VERSION="4.15.3";
+    analytics.load("${SEGMENT_KEY}");
+    analytics.page();
+    }}();`
+        }}
+      />
+    )}
+  </>
   )
 }
 
